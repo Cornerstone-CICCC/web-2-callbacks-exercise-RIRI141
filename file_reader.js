df59bc88f6081f04b907cb9417ea08afab974ba1
Ryoga <isimaruru.1712@gmail.com>
2024-11-19 12:06:32 -0800

    done

--- file_reader.js
+++ file_reader.js
@@ -2,4 +2,18 @@ const fs = require("fs");
 
 fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
   // your code here
-})
+  const word1 = data1;
+  fs.readFile("./lastname.txt", "utf-8", (err, data2) => {
+    const word2 = data2;
+    fs.readFile("./age.txt", "utf-8", (err, data3) => {
+      const word3 = data3;
+      fs.readFile("./hobbies.txt", "utf-8", (err, data4) => {
+        const arrayTostr = data4.replace(/[\[\]"]/g, "");
+        const word4 = arrayTostr.trim().split(",");
+        console.log(
+          `${word1} ${word2} is ${word3} years old and his hobbies are ${word4[0]} and${word4[1]}`
+        );
+      });
+    });
+  });
+});
